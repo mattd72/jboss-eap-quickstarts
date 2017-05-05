c4323031cfc6757b22b813aa19ffecedaf6681a8
matt <mattd@redhat.com>
2017-05-05 16:50:23 -0400

    Update MemberRegistrationIT.java

--- kitchensink/src/test/java/org/jboss/as/quickstarts/kitchensink/test/MemberRegistrationIT.java
+++ kitchensink/src/test/java/org/jboss/as/quickstarts/kitchensink/test/MemberRegistrationIT.java
@@ -58,7 +58,7 @@ public class MemberRegistrationIT {
         newMember.setName("Jane Doe");
         newMember.setEmail("jane@mailinator.com");
         newMember.setPhoneNumber("2125551234");
-        memberRegistration.register(newMember);
+        //memberRegistration.register(newMember);
         assertNotNull(newMember.getId());
         log.info(newMember.getName() + " was persisted with id " + newMember.getId());
     }
